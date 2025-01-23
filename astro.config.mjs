8f5bcd2c9172393ef364b6336acc0f958bfba651
PaulieScanlon <pauliescanlon@gmail.com>
2025-01-23 15:45:21 +0000

    chore: add action and CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 export default defineConfig({
+  site: 'https://databranching.co',
   integrations: [
     mdx({
       rehypePlugins: [
